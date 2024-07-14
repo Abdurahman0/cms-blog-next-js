b889c2d476b1db69d6928efa9fca56396a54e59d
Dumbie <wasleyron668@gmail.com>
2024-07-14 11:26:35 +0500

    Layout  Navbar Footer

---
+++ app/(root)/_components/mobile.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import { navLinks } from "@/constants"
+import { cn } from "@/lib/utils"
+import { Separator } from "@radix-ui/react-separator"
+import { Menu} from "lucide-react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const Mobile = () => {
+  const pathname = usePathname()
+
+  return (
+    <Sheet>
+  <SheetTrigger asChild className="flex md:hidden">
+    <Button size={'icon'} variant={'ghost'}>
+        <Menu/>
+    </Button>
+  </SheetTrigger>
+  <SheetContent side={'left'}>
+    <Link href={'/'}>
+      <h1 className="text-4-xl font-creteRound">Sammi</h1>
+    </Link>
+    <Separator/>
+    <div className="flex flex-col space-y-3">
+        {navLinks.map(nav => (
+            <Link 
+              key={nav.route} 
+              href={nav.route} 
+              className={cn('hover:bg-blue-400/20 py-2 px-3 cursor-pointer rounded-sm transition-colors flex items-center gap-2',
+                pathname === nav.route && 'text-blue-400 bg-blue-400/20'
+              )}
+            >
+                <nav.icon className="w-5 h-5" />
+                {nav.name}
+            </Link>
+        ))}
+    </div>
+  </SheetContent>
+</Sheet>
+
+  )
+}
+
+export default Mobile
